8d3adcb67dd9c72a858a99b314dd39d9f4bf4f6e
Spartex <vedantgarode11@gmail.com>
2023-09-29 00:50:50 +0530

    2.0

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 from fastapi import FastAPI
+import openai
+
 app = FastAPI()
+
 @app.get("/")
 async def root():
    return {"message": "Hello World"}
+
+
+@app.get("/convo")
+async def convo():
+   os.environ["OPENAI_API_KEY"] = "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"
+   response = openai.ChatCompletion.create(
+      model="gpt-3.5-turbo",
+      messages=[
+         {"role": "system", "content": "You are a helpful assistant."},
+         {"role": "user", "content": "Who won the world series in 2020?"},
+         {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+         {"role": "user", "content": "Where was it played?"}
+      ]
+   )
+
+print(response['choices'][0]['message']['content'])
